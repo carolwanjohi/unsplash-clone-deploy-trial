b38e6b1c0fa94467f3fc3fd710bb9503a4e7af7f
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-11-19 16:39:21 +0300

    Try to deploy Unsplash clone

---
+++ clone/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    url( r'^$', views.index, name="index"),
+    url( r'^tag/(\d+)', views.tag, name="tag"),
+    url( r'^image-post/(\d+)', views.image_post, name="imagePost"),
+    url( r'^search/', views.search_results, name="search_results"),
+    url( r'^collections/', views.collections, name="collections")
+]
+
+if settings.DEBUG:
+    urlpatterns += static( settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
+    
